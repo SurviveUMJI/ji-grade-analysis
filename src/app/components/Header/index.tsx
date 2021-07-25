98a81b0205fe8bb3c037069780441393ba083d4c
BoYanZh <bomingzh@sjtu.edu.cn>
2021-07-26 03:56:17 +0800

    feat: add link to title

--- src/app/components/Header/index.tsx
+++ src/app/components/Header/index.tsx
@@ -1,9 +1,6 @@
 import * as React from 'react';
-import {
-  Grid,
-  Typography,
-} from '@material-ui/core';
-import {GitHubButton, GitHubButtonProvider} from 'react-github-button';
+import { Grid, Typography, Link } from '@material-ui/core';
+import { GitHubButton, GitHubButtonProvider } from 'react-github-button';
 import 'react-github-button/assets/style.less';
 
 export interface HeaderProps {
@@ -21,22 +18,35 @@ export class Header extends React.Component<HeaderProps, HeaderState> {
     return (
       <div>
         <Grid container justifyContent="center">
-          <Typography component="h1" variant="h3" color="inherit"
-                      gutterBottom align={'center'}>
-            UM-SJTU JI Grade Analysis
+          <Typography
+            component="h1"
+            variant="h3"
+            color="inherit"
+            gutterBottom
+            align={'center'}
+          >
+            <Link
+              href="/#/courses"
+              color="inherit"
+              style={{ textDecoration: 'none' }}
+            >
+              UM-SJTU JI Grade Analysis
+            </Link>
           </Typography>
         </Grid>
-        <GitHubButtonProvider namespace={this.props.namespace}
-                              repo={this.props.repo}>
+        <GitHubButtonProvider
+          namespace={this.props.namespace}
+          repo={this.props.repo}
+        >
           <Grid container justifyContent="flex-end" spacing={1}>
             <Grid item>
-              <GitHubButton type="stargazers" size="large"/>
+              <GitHubButton type="stargazers" size="large" />
             </Grid>
             <Grid item>
-              <GitHubButton type="watchers" size="large"/>
+              <GitHubButton type="watchers" size="large" />
             </Grid>
             <Grid item>
-              <GitHubButton type="forks" size="large"/>
+              <GitHubButton type="forks" size="large" />
             </Grid>
           </Grid>
         </GitHubButtonProvider>
