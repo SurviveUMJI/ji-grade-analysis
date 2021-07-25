fccccb89b50556505c9580916af3430adb3fd42c
BoYanZh <bomingzh@sjtu.edu.cn>
2021-07-26 03:51:09 +0800

    feat: add busuanzi

--- src/app/components/Footer/index.tsx
+++ src/app/components/Footer/index.tsx
@@ -1,22 +1,30 @@
 import * as React from 'react';
-import {Grid, Link} from '@material-ui/core';
+import { Grid, Link } from '@material-ui/core';
 import packageData from 'app/../../package.json';
 
-export interface FooterProps {
-}
+export interface FooterProps {}
 
-export interface FooterState {
-}
+export interface FooterState {}
 
 export class Footer extends React.Component<FooterProps, FooterState> {
   render() {
     return (
       <footer>
-        <br/>
-        <Grid container justifyContent="flex-end">
-          Version {packageData.version}, Powered by&nbsp;
-          <Link href="https://github.com/tc-imba" target="_blank">tc-imba</Link>,
-          Copyright 2019-2021
+        <br />
+        <Grid container justifyContent="space-between">
+          <div className="busuanzi">
+            <span id="busuanzi_container_site_pv">
+              Visitor count:&nbsp;
+              <span id="busuanzi_value_site_pv"></span>
+            </span>
+          </div>
+          <span>
+            Version {packageData.version}, Powered by&nbsp;
+            <Link href="https://github.com/tc-imba" target="_blank">
+              tc-imba
+            </Link>
+            , Copyright 2019-2021
+          </span>
         </Grid>
       </footer>
     );
